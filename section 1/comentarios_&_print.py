a55af58d297d45bd7efa62d6cfe4341ab81f8a7b
Luanjoss <luanmicro890@gmail.com>
2021-09-14 16:29:10 -0300

    novos arquivos

---
+++ section 1/comentarios_&_print.py
@@ -0,0 +1,18 @@
+#TIPOS DE COMENTÁRIOS
+
+# com o jogo da velha, você comenta em apenas uma linha
+
+"""
+
+com três aspas você tem o 
+comentario de multiplas linhas
+até fechar as aspas novamente.
+
+"""
+#IMPRIMIR ALGO NA TELA
+
+#print imprime a informação dentro do parenteses 
+
+print("Olá mundo!")
+print("Olá mundo?")
+print("Olá mundo.")
