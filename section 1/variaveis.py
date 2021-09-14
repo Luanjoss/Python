23076a5fe14da1272453a713f6d56b34bb39994e
Luanjoss <luanmicro890@gmail.com>
2021-09-14 16:57:26 -0300

    Adcionado novas linhas

--- section 1/variaveis.py
+++ section 1/variaveis.py
@@ -5,3 +5,36 @@ print(minha_variavel1)
 print(minha_variavel2)
 print(minha_variavel1)
 print(minha_variavel2)
+
+#TIPOS DE VARIÁVEIS
+
+"""
+
+NUMERO INTERIO
+
+48
+
+NUMÉRICO FLUTUANTE
+
+18.57
+
+TEXTUAL(STRING)
+
+"OLÁ MUNDO."
+
+BOOLEANA
+
+TRUE (VERDADEIRO) OU FLASE (FASLO).
+
+"""
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+varint = 1
+varfloat = 1.5
+varstring = "Eu  sou uma String"
+varboo = True
+
+print(varint)
+print(varfloat)
+print(varstring)
+print(varboo)
