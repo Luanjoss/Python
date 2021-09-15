390d7e3d290ca75310b8522c30ffd5acef7c2451
Luanjoss <luanmicro890@gmail.com>
2021-09-14 22:45:55 -0300

    Condicional 1

---
+++ section 1/comandos_condicionais.py
@@ -0,0 +1,16 @@
+"""
+COMANDOS CONDICIONAIS
+
+IF => "SE",Trabalha com condiçoes booleanas.
+
+"""
+
+x = 1
+y = 154
+
+if x > y:
+    print("x é maior que y")
+
+if y > x:
+    print("y é maior que x")
+
