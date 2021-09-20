8741e0b61c5952edf3ea01e8a9ebebcee7ada1e5
Luanjoss <luanmicro890@gmail.com>
2021-09-20 20:43:45 -0300

    add else

--- section 1/comandos_condicionais.py
+++ section 1/comandos_condicionais.py
@@ -7,7 +7,7 @@ Sixtaxe
 
 if condição:
     execute_esta_linha
-    
+
 """
 
 x = 1
@@ -19,3 +19,29 @@ if x > y:
 if y > x:
     print("y é maior que x")
 
+"""
+COMANDOS CONDICIONAIS
+
+else => Ele é executado quando o comando if não é verdadeiro.
+
+Sixtaxe
+
+if condição:
+    execute_esta_linha
+    caso contrário
+else:
+    execute_esta_linha
+    
+"""
+print("----------------------------------------------------------------------------")
+x = 1
+y = 2
+
+if x == y:
+    print("numeros iguais")
+elif x < y:
+    print("x menor que y")
+elif y > x:
+    print("numeros iguais")
+else:
+    print("x não é maior que y")
